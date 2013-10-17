07810672daf660d4c97e99d2e4bb0958a7e0d8d8
Pauli Perälä <perala.pauli@gmail.com>
2013-10-17 18:38:29 +0300

    removed nodejs asserts, added mocha tests
    Mocha generates more structure and clearer reports

--- scripts/tests/arrayTest.js
+++ scripts/tests/arrayTest.js
@@ -1,28 +1,115 @@
 var MyArray = require('../array')
-var assert = require('assert')
+var chai = require('chai')
+var expect = chai.expect
 var array = new MyArray
 
 
-assert(array.isEmpty(),"Array should be empty at the beginning")
-var empty = {}
-array.insert(empty)
-assert(!array.isEmpty(),"Array should not be empty if empty object inserted")
-assert.strictEqual(1, array.length, "Array length should be 1 after empty object inserted")
-assert.strictEqual(empty, array.get(0),"Array should return empty object")
-assert.throws(function(){array.remove("0")},Error, "Array should throw error if you give it string as parameter when invoking remove")
-array.remove(0);
-assert(array.isEmpty(),"Array shoul be empty after inserting 1 and then removing 1")
-assert.throws(function(){array.remove(0)}, Error, "Array should throw new error when trying to remove from empty array")
-array.insert(1)
-array.insert(2)
-array.insert(3)
-
-array.remove(1)
-assert.equal(array.get(0),1)
-assert.equal(array.get(1),3)
-assert.equal(array.length,2)
 
 
-describe('testing testins', function(){
-	it ('hashas')
+
+describe('Array', function(){
+	beforeEach(function(){
+		array = new MyArray
+	})
+	describe('#isEmpty()', function(){
+		it('true initially', function(){
+			expect(array.isEmpty()).to.be.true
+		})
+		it('false after inserting', function(){
+			array.insert({})
+			expect(array.isEmpty()).to.be.false
+		})
+	})
+	describe('length', function(){
+		it('0 at the beginning', function(){
+			expect(array.length).to.eq(0)
+		})
+		it('1 after inserting', function(){
+			array.insert({})
+			expect(array.length).to.eq(1)
+		})
+		it('0 after inserting and removing', function(){
+			array.insert({})
+			array.remove(0)
+			expect(array.length).to.eq(0)
+		})
+		it ('1 after inserting 2 and removing 1', function(){
+			array.insert({})
+			array.insert({})
+			array.remove(1)
+			expect(array.length).to.eq(1)
+		})
+	})
+	describe('#insert', function(){
+		describe('allows', function(){
+			function insert(val){
+				expect(function(){array.insert(val)}).to.not.throw()
+			}
+			it('number', function(){
+				insert(1)
+			})
+			it('array', function(){
+				insert(new Array)
+				insert([1,2,3,4])
+			})
+			it('object', function(){
+				insert({key:'value'})
+			})
+			it('string', function(){
+				insert('asd')
+			})
+			it('boolean', function(){
+				insert(true)
+			})
+			it('date', function(){
+				insert(new Date)
+			})
+			it('regex', function(){
+				insert(/testings/)
+			})
+			it('itself', function(){
+				insert(array)
+			})
+		})
+
+	})
+	describe('#remove', function(){
+
+	})
+	describe('#get', function(){
+		it('error if outOfBounds', function(){
+			expect(function(){array.get(0)}).to.throw()
+		})
+		it('return correct element', function(){
+			array.insert(0)
+			array.insert(1)
+			expect(array.get(0)).to.eq(0)
+			expect(array.get(1)).to.eq(1)
+		})
+		it('return itself if inserted and retrieved',function(){
+			array.insert(array)
+			expect(array.get(0)).to.eq(array)
+		})
+		it('work with array[0] -syntax', function(){
+			var empty = {}
+			array.insert(empty)
+			expect(array[0]).to.eq(empty)
+		})
+
+	})
+	describe('#swap', function(){
+		it('2 elements', function(){
+			array.insert(0)
+			array.insert(1)
+			array.swap(0,1)
+			expect(array.get(0)).to.eq(1)
+			expect(array.get(1)).to.eq(0)
+		})
+	})
+	describe('#contains', function(){
+
+	})
+	describe('#indexOf',function(){
+
+	})
 })
--- scripts/tests/setTest.js
+++ scripts/tests/setTest.js
@@ -1,11 +1,84 @@
-assert = require ('assert')
-set = require('../set')()
+var assert = require ('assert')
+var chai = require('chai')
+var expect = chai.expect
+var Set = require('../set')
+var set = Set()
 
+function insertMany(){
+	for(var i =0;i<50;i++){
+		set.insert(i)
+	}
+}
 
-assert(set.isEmpty(), 'Set should beEmpty before adding any members')
-set.insert(1)
-assert(!set.isEmpty(), 'Set should not be empty after 1 added')
+describe('Set', function(){
+	beforeEach(function(){
+		set = new Set
+	})
+	describe('#insert', function(){
+		it('returns true if inserting 1', function(){
+			expect(set.insert(1)).to.eq(true)
+		})
+		it('return false if trying to insert same', function(){
+			set.insert(1)
+			set.insert(2)
+			expect(set.insert(1)).to.eq(false)
+		})
+	})
+	describe('#contains', function(){
+		it ('false if empty', function(){
+			expect(set.contains(1)).to.be.false
+		})
+		it('true if contained', function(){
+			set.insert(1)
+			expect(set.contains(1)).to.be.true
+		})
+		it('false if not contained', function(){
+			set.insert(1)
+			set.insert(3)
+			expect(set.contains(2)).to.be.false
+		})
+		describe('after 50 inserted', function(){
+			beforeEach(insertMany)
+			it('false if not present', function(){
+				expect(set.contains(50)).to.be.false
+				expect(set.contains(-1)).to.be.false
+			})
+			it ('true if present', function(){
+				for(var i =0;i<50;i++){
+					expect(set.contains(i)).to.be.true
+				}
+			})
+		})
+	})
+	describe('#remove', function(){
+		it ('false if empty', function(){
+			expect(set.remove(1)).to.be.false
+		})
+		it ('true if only contained', function(){
+			set.insert(1)
+			expect(set.remove(1)).to.be.true
+		})
+		it ('true if one of many contained', function(){
+			insertMany()
+			expect(set.remove(25)).to.be.true
+		})
+	})
+	describe('#size', function(){
+		it('0 if empty', function(){
+			expect(set.size()).to.eq(0)
+		})
+		it ('1 after 1 added', function(){
+			set.insert(1)
+			expect(set.size()).to.eq(1)
+		})
+		it ('0 after 1 added and removed', function(){
+			set.insert(1)
+			set.remove(1)
+			expect(set.size()).to.eq(0)
+		})
+	})
+})
 
-assert.equal(set.size(),1,'set size should be 1 if 1 added')
-
-assert(set.contains(1), 'set should contain the only member it was added')
+function print(){
+	console.log(set.toString())
+}
