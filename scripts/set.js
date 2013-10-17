89696254c0ea5a6bdeeba41867225c3f8b514506
Pauli Perälä <perala.pauli@gmail.com>
2013-10-17 18:37:20 +0300

    removed fake set, updated tree set a bit
    things still missing:
    *removing if not leaf
    *balancing (for AVL)

--- scripts/set.js
+++ scripts/set.js
@@ -1,34 +1,9 @@
 function Set(compare){
-	return new FakeSet(compare);
-}
-function FakeSet(compare){
-	var list = []
-	compare =  compare||function(a, b){
-		if (typeof a !=='number' || typeof b !=='number'){
-			throw new Exception('You need to implement pass compare method in constructor for non-number '+
-				'objects. '+ typeof a + " "+typeof b)
-		}
-		return a-b
-	}
-
-	return {
-		insert:insert,
-		contains:contains,
-		remove:list.poll,
-		size:size,
-		isEmpty:isEmpty
-	}
-	function insert(elem){
-		list.push(elem)
-	}
-	function contains(element){
-		return list.indexOf(element)!=-1
-	}
-	function size(){return list.length}
-	function isEmpty(){return size()==0}
+	return new TreeSet(compare);
 }
 function TreeSet(compare){
 	var size =0
+	var root
 	compare =  compare||function(a, b){
 		if (typeof a !=='number' || typeof b !=='number'){
 			throw new Exception('You need to implement pass compare method in constructor for non-number '+
@@ -43,28 +18,85 @@ function TreeSet(compare){
 		insert: insert,
 		contains: contains,
 		remove:remove,
-		size:count,
-		isEmpty: function(){return size==0}
+		size:function(){return size},
+		isEmpty: function(){return size==0}, 
+		toString:toString
 	}
 
-	function count(){
-		return size
+	function toString(){
+		return print(root)
+	}
+	function print(elem){
+		if (!elem){
+			return " " 
+		}
+		return print(elem.left)+elem.obj+print(elem.right)
 	}
 	function insert(val){
-		if (contains(val))
+		if (!root){
+			root = new Node(val)
+			size++
+			return true
+		}
+		var ret = insertTo(root, new Node(val))
+		if (ret)size++
+		return ret
+	}
+	function insertTo(cur, node){
+		var comp = compare(cur.obj,node.obj)
+		if(!comp){
 			return false
-		// @TODO insert
-		size+=1
+		}
+		if (comp<0){
+			if (!cur.left){
+				cur.left=node
+				node.parent=cur
+			}else
+				insertTo(cur.left,node)
+			
+
+		}else{
+			if (!cur.right){
+				cur.right=node
+				node.parent=cur
+			}else
+				insertTo(cur.right,node)
+		}
 		return true
 	}
 
-	function contains(){
-		return false
-		// @TODO implement
-	}
-	function remove(){
-		// @TODO implement	
+	function balance(elem){
 
+	}
+
+	function contains(elem){
+		return !!find(elem)
+	}
+	function find(elem){
+		var current = root
+		while (current){
+			var comp = compare(current.obj, elem)
+			if (!comp)
+				return current
+			if (comp<0)
+				current=current.left
+			else
+				current= current.right
+			
+		}
+	}
+	function remove(elem){
+		elem = find(elem)
+		if (!elem)return false
+		if (elem.parent && elem === elem.parent.right){
+			elem.parent.right=undefined
+			// @TODO handle children
+		}else if (elem.parent && elem === elem.parent.left){
+			elem.parent.left=undefined
+			// @TODO handle children
+		}
+		size--
+		return true 
 	}
 
 
